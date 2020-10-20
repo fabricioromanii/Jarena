9585e6dc66207b89102754840e2e43cb588640d9
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-10-20 15:00:01 -0300

    adicionando classe movimenta e um contador para o jogo

--- src/br/uffs/cc/jarena/JamesConde.java
+++ src/br/uffs/cc/jarena/JamesConde.java
@@ -1,61 +1,103 @@
-/**
- * Um exemplo de agente que anda aleatoriamente na arena. Esse agente pode ser usado como base
- * para a criação de um agente mais esperto. Para mais informações sobre métodos que podem
- * ser utilizados, veja a classe Agente.java.
- * 
- * Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
- */
-
 package br.uffs.cc.jarena;
 
 public class JamesConde extends Agente
 {
+
+	int MeuContador;
+	int MeuContadorAuxiliar;
+	
+    private int alvo_x;
+    private int alvo_y;
+
 	public JamesConde(Integer x, Integer y, Integer energia) {
 		super(x, y, energia);			
-		para();	}
-	
-	public void pensa() {
-		// Se não conseguimos nos mover para a direção atual, quer dizer
-		// que chegamos no final do mapa ou existe algo bloqueando nosso
-		// caminho.
-		if(!podeMoverPara(getDirecao())) {
-			// Como não conseguimos nos mover, vamos escolher uma direção
-			// nova.
-			setDirecao(geraDirecaoAleatoria());
-			System.out.println("James Conde vai mudar de direcao");
-		}
-		/*
-		if(podeDividir() == true && getEnergia() > 800 ) {
-			divide();
-			geraDirecaoAleatoria();
-		}*/
-		if(getDirecao() == 1 && getX() == 500){
-			setDirecao(4);
-		}
-		if (getY() == 500 && getX() == 520){
-			setDirecao(geraDirecaoAleatoria());
-		}
-		
-
-		if(getDirecao() == 4 && getY() == 500){
-			setDirecao(1);
-		}
-		// Se o agente conseguie se dividir (tem energia) e se o total de energia
-		// do agente é maior que 400, nos dividimos. O agente filho terá a metade
-		// da nossa energia atual.
-		
+		MeuContador = 5;
+		MeuContadorAuxiliar = 0 ;  
+		setDirecao(DIREITA);
+        this.alvo_x = -1;
+        this.alvo_y = -1;
 	}
 	
+	public void pensa() {/*
+		if (estou_tomando_dano)
+        {            
+            //entra no modo corrida
+			this.modo_fuga = true;
+		}*/
+		movimenta();
+		MeuContadorAuxiliar++;
+	}
+	
+	public void movimenta(){
+	//movimenta faz a movimentação do agente;
+		
+		if((MeuContadorAuxiliar % 4) == 0){ //em teoria, vai fazer com que so seja executado a cada 3 tempos;
+			if(MeuContador <= 33 ){
+				//pega o id de 5 a 33, que é o minimo e o maximo do id do meu agente.
+				if (getId() == MeuContador && getX()>10){
+					setDirecao(BAIXO);
+					System.out.println("Estou indo para baixo");
+				}
+				MeuContador++;
+			}
+		}
+
+		if(!podeMoverPara(getDirecao())) {
+			// Como não conseguimos nos mover, vamos escolher uma direção nova.
+			setDirecao(geraDirecaoAleatoria());
+			System.out.println("bati numa parede, mudando direcao!");	
+		}
+		
+		if(podeDividir() && getEnergia() >= 900 && getX() > 100 && getY() > 100) {
+			divide();
+			setDirecao(geraDirecaoAleatoria());
+		}
+
+		if((MeuContadorAuxiliar % 17) == 0 && isParado()){
+			setDirecao(geraDirecaoAleatoria());
+		}
+	}
+		
 	public void recebeuEnergia() {
 		// Invocado sempre que o agente recebe energia.
+			para();
 	}
 	
 	public void tomouDano(int energiaRestanteInimigo) {
 		// Invocado quando o agente está na mesma posição que um agente inimigo
 		// e eles estão batalhando (ambos tomam dano).
-		System.out.println("Estou tomando dano !!!!");
+
+		if(getEnergia() > energiaRestanteInimigo){
+			para();	
+			System.out.println("Agente "+getId()+ " esta tomando dano.");
+			}else{
+				setDirecao(geraDirecaoAleatoria());
+				System.out.println("Agente "+getId()+ " vai fugir da luta.");
+
+			}
+
 	}
-	
+	/*
+	public void MovePara(int x, int y){
+
+		this.alvo_x = x;
+		this.alvo_y = y;
+		
+		if(alvo_x > getX()){
+			setDirecao(DIREITA);
+		}
+		else if (alvo_x < getX()){
+			setDirecao(ESQUERDA);
+		}
+		else if (alvo_y > getY()){
+			setDirecao(BAIXO);
+		}
+		else(alvo_y < getY()){
+			setDirecao(CIMA);
+		}
+
+	}*/
+
 	public void ganhouCombate() {
 		// Invocado se estamos batalhando e nosso inimigo morreu.
 		System.out.println("Ganhei essa luta! Da proxima vez tente ser bom, nao seja apenas voce.");
@@ -63,9 +105,6 @@ public class JamesConde extends Agente
 	
 	public void recebeuMensagem(String msg) {
 		// Invocado sempre que um agente aliado próximo envia uma mensagem.
-		
-		
-		
 	}
 	
 	public String getEquipe() {
