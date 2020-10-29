2173915aac97c96dffafce4489e92fdc96208cfd
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-10-29 01:09:04 -0300

    adicionando mensagens que os agentes se trocam, eles comecam a conversar entre si e comunicar aonde ha energia

--- src/br/uffs/cc/jarena/JamesConde.java
+++ src/br/uffs/cc/jarena/JamesConde.java
@@ -1,40 +1,40 @@
 package br.uffs.cc.jarena;
 
-public class JamesConde extends Agente
-{
+public class JamesConde extends Agente {
 
 	int MeuContador;
 	int MeuContadorAuxiliar;
-	
-    private int alvo_x;
-    private int alvo_y;
+	public int alvo_x;
+	public int alvo_y;
+	public int distanciaX;
+	public int distanciaY;
+	int THC;
+	int THCaux;
 
 	public JamesConde(Integer x, Integer y, Integer energia) {
-		super(x, y, energia);			
+		super(x, y, energia);
 		MeuContador = 5;
-		MeuContadorAuxiliar = 0 ;  
-		setDirecao(DIREITA);
-        this.alvo_x = -1;
-        this.alvo_y = -1;
+		MeuContadorAuxiliar = 0;
+		setDirecao(BAIXO);
 	}
-	
-	public void pensa() {/*
-		if (estou_tomando_dano)
-        {            
-            //entra no modo corrida
-			this.modo_fuga = true;
-		}*/
+
+	public void pensa() {
+		// if (estou_tomando_dano) { //entra no modo corrida this.modo_fuga = true; }
 		movimenta();
 		MeuContadorAuxiliar++;
+
 	}
-	
-	public void movimenta(){
-	//movimenta faz a movimentação do agente;
-		
-		if((MeuContadorAuxiliar % 4) == 0){ //em teoria, vai fazer com que so seja executado a cada 3 tempos;
-			if(MeuContador <= 33 ){
-				//pega o id de 5 a 33, que é o minimo e o maximo do id do meu agente.
-				if (getId() == MeuContador && getX()>10){
+
+	public void movimenta() {
+		// movimenta faz a movimentação do agente;
+		if (getY() == 20 && getX() == 0) {
+			setDirecao(DIREITA);
+		}
+
+		if ((MeuContadorAuxiliar % 4) == 0) { // em teoria, vai fazer com que so seja executado a cada 3 tempos;
+			if (MeuContador <= 33) {
+				// pega o id de 5 a 33, que é o minimo e o maximo do id do meu agente.
+				if (getId() == MeuContador && getX() > 10) {
 					setDirecao(BAIXO);
 					System.out.println("Estou indo para baixo");
 				}
@@ -42,86 +42,87 @@ public class JamesConde extends Agente
 			}
 		}
 
-		if(!podeMoverPara(getDirecao())) {
+		if (!podeMoverPara(getDirecao())) {
 			// Como não conseguimos nos mover, vamos escolher uma direção nova.
 			setDirecao(geraDirecaoAleatoria());
-			System.out.println("bati numa parede, mudando direcao!");	
+			System.out.println("bati numa parede, mudando direcao!");
 		}
 		/*
-		if(podeDividir() && getEnergia() >= 900 && getX() > 100 && getY() > 100) {
-			divide();
-			setDirecao(geraDirecaoAleatoria());
-		}*/
-
-		if((MeuContadorAuxiliar % 16) == 0 && isParado()){
+		 * if(podeDividir() && getEnergia() >= 900 && getX() > 100 && getY() > 100) {
+		 * divide(); setDirecao(geraDirecaoAleatoria()); }
+		 */
+		if ((MeuContadorAuxiliar % 16) == 0 && isParado()) {
 			setDirecao(geraDirecaoAleatoria());
 		}
+		/*
+		 * if (getX() > 600 || getY() > 30) { MeuContador = 5;
+		 * 
+		 * if (MeuContador <= 33) { if (getId() == MeuContador) { THC = getX(); int
+		 * ContAux = MeuContador + 2; if (getId() == ContAux) { THCaux = getX(); } if
+		 * ((THC - THCaux) < 25) { setDirecao(geraDirecaoAleatoria());
+		 * System.out.println("temo junto"); } }
+		 * 
+		 * } MeuContador++; }
+		 */
 	}
-		
+
 	public void recebeuEnergia() {
 		// Invocado sempre que o agente recebe energia.
-			para();
-			System.out.println(getX() + "|" + getY());
-			(this.getX() +"|" + this.getY());
+		para();
+		System.out.println(getX() + "," + getY());
+		String ox = Integer.toString(getX());
+		String oy = Integer.toString(getY());
+		enviaMensagem(ox + "," + oy);
+
 	}
-	
+
 	public void tomouDano(int energiaRestanteInimigo) {
 		// Invocado quando o agente está na mesma posição que um agente inimigo
 		// e eles estão batalhando (ambos tomam dano).
 
-		if(getEnergia() > energiaRestanteInimigo){
-			para();	
-			System.out.println("Agente "+getId()+ " esta tomando dano.");
-			}else{
-				setDirecao(geraDirecaoAleatoria());
-				System.out.println("Agente "+getId()+ " vai fugir da luta.");
+		if (getEnergia() > energiaRestanteInimigo) {
+			para();
+			System.out.println("Agente " + getId() + " esta tomando dano.");
+		} else {
+			setDirecao(geraDirecaoAleatoria());
+			System.out.println("Agente " + getId() + " vai fugir da luta.");
 
-			}
-
+		}
 	}
-	
-	public void MovePara(int x, int y){
 
-		this.alvo_x = x;
-		this.alvo_y = y;
-		
-		if( getX() > alvo_x ){
+	public void MovePara(int x, int y) {
+
+		if (getX() > x) {
 			setDirecao(ESQUERDA);
-		}
-		else if ( getX() < alvo_x){
+		} else if (getX() < x) {
 			setDirecao(DIREITA);
-		}
-		else if ( getY() > alvo_y ) {
+		} else if (getY() > y) {
 			setDirecao(CIMA);
-		} 
-		else if( getY() < alvo_y){
+		} else if (getY() < y) {
 			setDirecao(BAIXO);
 		}
-		
+
 	}
 
 	public void ganhouCombate() {
 		// Invocado se estamos batalhando e nosso inimigo morreu.
 		System.out.println("Ganhei essa luta! Da proxima vez tente ser bom, nao seja apenas voce.");
 	}
-	
+
 	public void recebeuMensagem(String msg) {
 		// Invocado sempre que um agente aliado próximo envia uma mensagem.
-		String aa = msg; 
-		String Coordenadas[] = new String[2];
-		//100|200|3000|Agente02
-		Coordenadas = aa.split("|");
-		//100 [0]
-		//200 [1]
-		
-		int meuX =Integer.parseInt(Coordenadas[0]);
-		int meuY =Integer.parseInt(Coordenadas[1]);
-		//100 int 
-		//200 int
-		MovePara(meuX,meuY);
-		
+
+		String[] Coordenadas = msg.split(",");
+		int meuX = Integer.parseInt(Coordenadas[0]);
+		int meuY = Integer.parseInt(Coordenadas[1]);
+
+		// System.out.println(meuX);
+		// System.out.println(meuY);
+
+		MovePara(meuX, meuY);
+
 	}
-	
+
 	public String getEquipe() {
 		// Definimos que o nome da equipe do agente é "James Conde".
 		return "James Conde";
